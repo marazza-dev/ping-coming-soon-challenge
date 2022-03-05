5888c361b2cf9bbdfcb87c14b1ebe54f8882af95
Federico Marazzani <marazzani.dev@gmail.com>
2022-03-05 19:13:20 +0100

    add components and App markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.scss";
+import logo from "./images/logo.svg";
+import heroImg from "./images/illustration-dashboard.png";
+
+import { Header, Form, Footer } from "./components";
 
 const App = () => {
-  return <div>Hello!</div>;
+  return (
+    <main>
+      <img src={logo} alt='ping brand logo' />
+      <Header />
+      <Form />
+      <img src={heroImg} alt='illustrated dashboard' />
+      <Footer />
+    </main>
+  );
 };
 
 export default App;
