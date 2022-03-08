3e1f11020466432e4d9dc4b5fc24d8b307b1f37e
Federico Marazzani <marazzani.dev@gmail.com>
2022-03-08 12:18:19 +0100

    update Form :: add styles

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
 import "./form.scss";
+import FormSubscribe from "./FormSubscribe";
+import FormSuccess from "./FormSuccess";
 
 const Form = () => {
-  return <div>Form</div>;
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function submitForm() {
+    setIsSubmitted(true);
+  }
+  function goBack() {
+    setIsSubmitted(false);
+  }
+
+  return (
+    <>
+      {!isSubmitted ? (
+        <FormSubscribe submitForm={submitForm} />
+      ) : (
+        <FormSuccess goBack={goBack} />
+      )}
+    </>
+  );
 };
 
 export default Form;
